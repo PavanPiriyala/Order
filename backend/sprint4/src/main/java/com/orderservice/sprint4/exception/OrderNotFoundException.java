43b956e8e13ab9ce54be5b3ea0c8e07d9c203c13
Akram Raza Shaik <akshaik@nisum.com>
2025-07-10 10:02:55 +0530

    User and pdf exceptions

--- backend/sprint4/src/main/java/com/orderservice/sprint4/exception/OrderNotFoundException.java
+++ backend/sprint4/src/main/java/com/orderservice/sprint4/exception/OrderNotFoundException.java
@@ -1,8 +1,12 @@
 package com.orderservice.sprint4.exception;
 
 public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
     public OrderNotFoundException(Integer orderId) {
-        super("Order with ID " + orderId + " not found.");
+        super("Order with ID " + orderId + " not found");
     }
 }
 
